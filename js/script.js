cf7293e9755b48a3095e9cc1f651f6632a85206b
RyoUsj <54502176+RyoUsj@users.noreply.github.com>
2020-12-07 11:22:08 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,41 +7,15 @@ $(function(){
 });
 
 var swiper = new Swiper('.swiper-container', {
-  loop: true,
-      // loopedSlides: 8,
-      centeredSlides: true,
-			slidesPerView: 'auto',
-      spaceBetween: 56,
-  // loop: true, // ループ処理
-  // slidesPerView: 'auto', // ※※※重要 無限ループ※※※ （''内をautoにすると1番目から始まる。）
-  // centeredSlides : true,
-  // breakpoints: {
-  //   375: {
-  //     slidesPerView: 1.2,
-  //     spaceBetween: 8,
-  //   },
-  //   768: {
-  //     slidesPerView: 2.1,
-  //     spaceBetween: 16,
-  //   },
-  //   992: {
-  //     slidesPerView: 3.2,
-  //     spaceBetween: 32,
-  //   },
-  //   1200: {
-  //     slidesPerView: 3.8,
-  //     spaceBetween: 56,/* カードの間隔 */
-  //   },
-  // },
-  // slidesPerView: 3.8,
-  // spaceBetween: 56,/* カードの間隔 */
-  // initialSlide: 1,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 56,
+  simulateTouch: true,
   loop: true,
   autoplay: {
     delay: 5000,/* 動作時間 */
     disableOnInteraction: false
   },
-  simulateTouch: true
 });
 
 new WOW().init();
@@ -49,7 +23,7 @@ new WOW().init();
 // #から始まるURLがクリックされた時
 $('a[href^="#"]').click(function() {
   // .headerクラスがついた要素の高さを取得
-  let header = $(".header").innerHeight();
+  let header = $("header").innerHeight();
   let speed = 500;
   let id = $(this).attr("href");
   let target = $("#" == id ? "html" : id);
